e1ef5b748488053a0f036da8252b4505af424656
agrad0 <grad.adam.j@gmail.com>
2022-08-16 22:20:50 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let firstNumber = 3;
+let secondNumber = 5;
+function add(firstNumber, secondNumber){
+    let result = 4 + 6;
+    return result;
+}
+console.log(add(firstNumber, secondNumber) );   
